052b6fa45b53124c0587396e55e414a786317e55
ACGaming <4818419+ACGaming@users.noreply.github.com>
2023-02-24 22:29:39 +0100

    Revert workaround, ensure first distance is far enough

--- src/main/java/com/smashingmods/reroll/events/PlayerLoginEvent.java
+++ src/main/java/com/smashingmods/reroll/events/PlayerLoginEvent.java
@@ -7,11 +7,16 @@ import com.smashingmods.reroll.config.Config;
 import com.smashingmods.reroll.handler.InventoryHandler;
 import com.smashingmods.reroll.handler.RerollHandler;
 import com.smashingmods.reroll.item.DiceItem;
+import com.smashingmods.reroll.util.PositionUtil;
+
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.CooldownTracker;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
@@ -31,6 +36,8 @@ public class PlayerLoginEvent {
             // Set tag to prevent initial settings every login
             event.player.getTags().add("joined");
 
+            setPlayerToBlock(event.player);
+
             if (event.player instanceof EntityPlayerMP) {
                 EntityPlayerMP entityPlayer = (EntityPlayerMP) event.player;
 
@@ -49,12 +56,16 @@ public class PlayerLoginEvent {
                         rerollCapability.setLock(true);
                     }
                 }
-
-                RerollHandler handler = new RerollHandler();
-                handler.resetLocation(entityPlayer.getServer(), entityPlayer, true);
-                handler.resetLocation(entityPlayer.getServer(), entityPlayer, true);
             }
         }
+    }
+
+    private static void setPlayerToBlock(EntityPlayer player) {
+        World world = player.getEntityWorld();
+        BlockPos currentPosition = player.getPosition();
+        currentPosition = currentPosition.add(currentPosition.getX() > 0 ? Config.minDistance : -Config.minDistance, 0, currentPosition.getZ() > 0 ? Config.minDistance : -Config.minDistance);
+        Optional<BlockPos> spawnBlock = PositionUtil.findClosest(currentPosition, Config.horizontalRange, Config.verticalRange, PositionUtil.blockStatePredicate(world));
+        spawnBlock.ifPresent(blockPos -> player.setPositionAndUpdate(blockPos.getX() + 0.5d, blockPos.getY() + 1.5d, blockPos.getZ() + 0.5d));
     }
 
     private static void setInitialCooldown(EntityPlayerMP entityPlayer) {
